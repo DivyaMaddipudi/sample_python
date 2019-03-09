819eb28eb67cf7e80c7dd9b1fd0763fe2530a25a
SatyaDivya <divya.maddipudi1@gmail.com>
2019-03-09 21:50:23 +0530

    python programms added

---
+++ python-50/27.py
@@ -0,0 +1,9 @@
+def check(n):
+
+    if n.isdigit():
+        print(n,"is a number")
+    else:
+        print(n,"is not a number")
+
+check("900")
+
---
+++ python-50/30.py
@@ -0,0 +1,12 @@
+def occurances(item):
+    count = 0
+    nl =[]
+    l1 = ["java","python","r","java","python","java"]
+    for i in l1:
+        if item == i:
+
+            count = count + 1
+
+    print(count)
+occurances("python")
+
